484b3d05d9a828f2e6b184ad260cd6db0a27b805
skelmis <ethan@koldfusion.xyz>
2022-08-22 20:26:03 +1200

    feat: Add Case inline on admin form

--- base/admin.py
+++ base/admin.py
@@ -3,12 +3,18 @@ from django.contrib import admin
 from base.models import Case, CodeError, DiscordUser
 
 
+class CodeErrorInline(admin.TabularInline):
+    model = CodeError
+    extra = 1
+
+
 @admin.register(Case)
 class CaseAdmin(admin.ModelAdmin):
     list_display = ("slug", "created_for", "created_at", "last_edited_at")
     readonly_fields = ("slug", "created_for", "created_at", "last_edited_at")
     list_filter = ("created_at", "last_edited_at", "created_for")
     search_fields = ("slug",)
+    inlines = (CodeErrorInline,)
 
 
 @admin.register(CodeError)
